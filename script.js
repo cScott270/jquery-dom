ec128bb0951fef44c33344bf41715b3e39ccada1
Connor <connor.q.scott270@gmail.com>
2023-11-26 09:24:29 -0600

    all but sort

---
+++ script.js
@@ -0,0 +1,15 @@
+// // 1.
+// $('document').ready(function () {
+//   console.log("Let's get ready to party with jQuery!");
+// });
+
+// // 2.
+// $('article img').addClass('image-center');
+
+// // 3.
+// $('p').last().remove();
+
+// // 4.
+// $('h1').css('font-size', `${Math.floor(Math.random() * 101)}px`);
+
+// // 5.
